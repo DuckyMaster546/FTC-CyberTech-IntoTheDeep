213abc25a2f7c341f68cbcb2cddf5417b34fee49
Kidnapper <90580198+DuckyMaster546@users.noreply.github.com>
2024-11-12 19:55:53 -0800

    Mecanum Driving Update

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleopfr.java
@@ -0,0 +1,73 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.robotcore.external.Func;
+import java.util.Base64;
+
+
+@TeleOp(name = "TeleOp")
+public class teleopfr extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //Drive Motors
+        DcMotorEx motorFrontLeft = hardwareMap.get(DcMotorEx.class, "LFMotor");
+        DcMotorEx motorBackLeft = hardwareMap.get(DcMotorEx.class, "LBMotor");
+        DcMotorEx motorFrontRight = hardwareMap.get(DcMotorEx.class, "RFMotor");
+        DcMotorEx motorBackRight = hardwareMap.get(DcMotorEx.class, "RBMotor");
+
+        motorFrontRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        motorBackRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        motorFrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorBackLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        motorBackRight.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        motorFrontLeft.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        motorFrontRight.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        motorBackLeft.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+
+        // Left
+        // Control Hub
+        // - Port 0: LBMotor
+        // - Port 1: LFMotor
+
+        // Right
+        // Expansion Hub
+        // - Port 0: RBMotor
+        // - Port 1: RFMotor
+
+        //speed adjust thing
+        double speedAdjust = 1;
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            // gamepad controls
+            double y = -gamepad2.left_stick_y;
+            double x = gamepad2.left_stick_x * -1; // Invert the x-axis input
+            double rx = gamepad2.right_stick_x;
+
+            // Calculate motor power with corrected x input for strafing
+            double frontRightPower = y - x - rx;
+            double frontLeftPower = y + x + rx;
+            double backLeftPower = y - x + rx;
+            double backRightPower = y + x - rx;
+
+            // Set motor power with corrected values for strafing direction
+            motorBackLeft.setPower(backLeftPower * speedAdjust);
+            motorBackRight.setPower(backRightPower * speedAdjust);
+            motorFrontLeft.setPower(frontLeftPower * speedAdjust);
+            motorFrontRight.setPower(frontRightPower * speedAdjust);
+
+        }
+
+    }
+}
+//}
